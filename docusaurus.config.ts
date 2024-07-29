6ab845eb619d74afa3de6dbfe776880c2c3588f6
hawkeyexl <hawkeyexl@gmail.com>
2024-07-29 03:21:51 -0700

    Formatted

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,54 +1,54 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'Doc Detective',
-  tagline: 'Keep your docs up to date with ease.',
-  favicon: 'img/favicon.ico',
+  title: "Doc Detective",
+  tagline: "Keep your docs up to date with ease.",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://doc-detective.com',
+  url: "https://doc-detective.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Doc Detective', // Usually your GitHub org/user name.
-  projectName: 'doc-detective.github.io', // Usually your repo name.
+  organizationName: "Doc Detective", // Usually your GitHub org/user name.
+  projectName: "doc-detective.github.io", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/doc-detective/doc-detective.github.io/tree/main/',
+            "https://github.com/doc-detective/doc-detective.github.io/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/doc-detective/doc-detective.github.io/tree/main/',
+            "https://github.com/doc-detective/doc-detective.github.io/tree/main/",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -56,63 +56,63 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'Doc Detective',
+      title: "Doc Detective",
       logo: {
-        alt: 'Doc Detective Logo',
-        src: 'img/logo.svg',
+        alt: "Doc Detective Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'docsSidebar',
-          position: 'left',
-          label: 'Docs',
+          type: "docSidebar",
+          sidebarId: "docsSidebar",
+          position: "left",
+          label: "Docs",
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/doc-detective',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/doc-detective",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Get started',
-              to: '/docs/get-started',
+              label: "Get started",
+              to: "/docs/get-started",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Discord',
-              href: 'https://discord.gg/2M7wXEThfF',
+              label: "Discord",
+              href: "https://discord.gg/2M7wXEThfF",
             },
             {
-              label: 'LinkedIn',
-              href: 'https://www.linkedin.com/company/doc-detective',
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/company/doc-detective",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             // {
             //   label: 'Blog',
             //   to: '/blog',
             // },
             {
-              label: 'GitHub',
-              href: 'https://github.com/doc-detective',
+              label: "GitHub",
+              href: "https://github.com/doc-detective",
             },
           ],
         },
