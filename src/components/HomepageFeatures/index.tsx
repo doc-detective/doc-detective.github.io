cf864d7c58d0c9640522ff8ad50a420d5575d358
AK <89195061+anaxite@users.noreply.github.com>
2025-02-24 23:15:23 +0100

    Format JS/JSX/CSS with Biome
    Some line rearranging
    Import sorting
    Add/remove characters as needed

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,70 +1,70 @@
-import clsx from "clsx";
 import Heading from "@theme/Heading";
+import clsx from "clsx";
 import styles from "./styles.module.css";
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<"svg">>;
-  description: JSX.Element;
+	title: string;
+	Svg: React.ComponentType<React.ComponentProps<"svg">>;
+	description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: "Keep procedures, screenshots, and videos current",
-    Svg: require("@site/static/img/ui.svg").default,
-    description: (
-      <>
-        Automate freshness checks for your procedures. Make sure page elements
-        say what they should and update your media all in one go.
-      </>
-    ),
-  },
-  {
-    title: "Make sure your requests return what you expect",
-    Svg: require("@site/static/img/api.svg").default,
-    description: (
-      <>
-        Use the same checks on your APIs as you do on your UI. Make sure your
-        APIs return the right data, and that it's formatted correctly.
-      </>
-    ),
-  },
-  {
-    title: "Catch surprises before your users do",
-    Svg: require("@site/static/img/cli.svg").default,
-    description: (
-      <>
-        Test your CLIs and SDKs like you do everything else. Run whatever
-        commands you need and compare the output to what you expect.
-      </>
-    ),
-  },
+	{
+		title: "Keep procedures, screenshots, and videos current",
+		Svg: require("@site/static/img/ui.svg").default,
+		description: (
+			<>
+				Automate freshness checks for your procedures. Make sure page elements
+				say what they should and update your media all in one go.
+			</>
+		),
+	},
+	{
+		title: "Make sure your requests return what you expect",
+		Svg: require("@site/static/img/api.svg").default,
+		description: (
+			<>
+				Use the same checks on your APIs as you do on your UI. Make sure your
+				APIs return the right data, and that it's formatted correctly.
+			</>
+		),
+	},
+	{
+		title: "Catch surprises before your users do",
+		Svg: require("@site/static/img/cli.svg").default,
+		description: (
+			<>
+				Test your CLIs and SDKs like you do everything else. Run whatever
+				commands you need and compare the output to what you expect.
+			</>
+		),
+	},
 ];
 
 function Feature({ title, Svg, description }: FeatureItem) {
-  return (
-    <div className={clsx("col col--4")}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={clsx("col col--4")}>
+			<div className="text--center">
+				<Svg className={styles.featureSvg} role="img" />
+			</div>
+			<div className="text--center padding-horiz--md">
+				<Heading as="h3">{title}</Heading>
+				<p>{description}</p>
+			</div>
+		</div>
+	);
 }
 
 export default function HomepageFeatures(): JSX.Element {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.features}>
+			<div className="container">
+				<div className="row">
+					{FeatureList.map((props, idx) => (
+						<Feature key={idx} {...props} />
+					))}
+				</div>
+			</div>
+		</section>
+	);
 }
--- src/components/JsonYamlTabs/index.jsx
+++ src/components/JsonYamlTabs/index.jsx
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
-import { Tabs, Tab } from 'react-bootstrap';
-import yaml from 'yaml';
+import React, { useState } from "react";
+import { Tab, Tabs } from "react-bootstrap";
+import yaml from "yaml";
 
 const JsonYamlTabs = ({ jsonInput }) => {
-    const [yamlOutput, setYamlOutput] = useState('');
+	const [yamlOutput, setYamlOutput] = useState("");
 
-    const handleTabSelect = (key) => {
-        if (key === 'yaml') {
-            const parsedJson = JSON.parse(jsonInput);
-            const yamlString = yaml.stringify(parsedJson);
-            setYamlOutput(yamlString);
-        }
-    };
-// Change to use Docusaurus code block
-    return (
-        <Tabs defaultActiveKey="json" onSelect={handleTabSelect}>
-            <Tab eventKey="json" title="JSON">
-                <pre>{jsonInput}</pre>
-            </Tab>
-            <Tab eventKey="yaml" title="YAML">
-                <pre>{yamlOutput}</pre>
-            </Tab>
-        </Tabs>
-    );
+	const handleTabSelect = (key) => {
+		if (key === "yaml") {
+			const parsedJson = JSON.parse(jsonInput);
+			const yamlString = yaml.stringify(parsedJson);
+			setYamlOutput(yamlString);
+		}
+	};
+	// Change to use Docusaurus code block
+	return (
+		<Tabs defaultActiveKey="json" onSelect={handleTabSelect}>
+			<Tab eventKey="json" title="JSON">
+				<pre>{jsonInput}</pre>
+			</Tab>
+			<Tab eventKey="yaml" title="YAML">
+				<pre>{yamlOutput}</pre>
+			</Tab>
+		</Tabs>
+	);
 };
 
 export default JsonYamlTabs;
