babe4d86725229f762758ddbf8769c24d223e932
hawkeyexl <hawkeyexl@gmail.com>
2025-10-20 18:57:46 -0400

    Add DITA markup coverage, fixed build issues

--- src/plugins/webpack-browserify.js
+++ src/plugins/webpack-browserify.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = function (context, options) {
     return {
       name: 'webpack-browserify',
@@ -13,8 +15,18 @@ module.exports = function (context, options) {
               http: require.resolve('stream-http'),
               https: require.resolve('https-browserify'),
               os: require.resolve('os-browserify/browser'),
+              crypto: require.resolve('crypto-browserify'),
+              buffer: require.resolve('buffer/'),
+              stream: require.resolve('stream-browserify'),
+              vm: require.resolve('vm-browserify'),
             },
           },
+          plugins: [
+            new webpack.ProvidePlugin({
+              Buffer: ['buffer', 'Buffer'],
+              process: 'process/browser',
+            }),
+          ],
         };
       },
     };
